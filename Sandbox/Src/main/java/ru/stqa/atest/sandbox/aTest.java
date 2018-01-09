914b3a22eace038a2a0d0d74a8ee50433ac0d3b8
mlaptev <mlaptev@issart.com>
2018-01-09 11:02:08 +0600

    Добавлено несколько функций

--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
@@ -1,10 +1,31 @@
 package ru.stqa.atest.sandbox;
 
+import javafx.scene.media.SubtitleTrack;
+
+import java.net.SocketPermission;
+
 public class aTest {
 
-    static public void main(String[] args) {
-        System.out.println("Hello, Wolder");
+    public static void main(String[] args) {
+
+        hello("world");
+        hello("user");
+        hello("MiQAVred");
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a , b));
+    }
+    public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
     }
 
-
+    public static double area (double len) {
+        return len * len;
+    }
+    public static double area (double a, double b) {
+      return a * b;
+    }
 }

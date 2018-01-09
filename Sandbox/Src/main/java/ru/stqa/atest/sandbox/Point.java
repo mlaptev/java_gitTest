4a7fc586ccfb6cc5fd15897c569513bdc7c973f1
mlaptev <mlaptev@issart.com>
2018-01-09 18:33:29 +0600

    автотестирование

---
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.atest.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double area () {
+        return this.x * this.y;
+    }
+}
+
+
--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
@@ -11,21 +11,20 @@ public class aTest {
         hello("world");
         hello("user");
         hello("MiQAVred");
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a , b));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 7);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p = new Point( 2, 4);
+                System.out.println("Координаты точек = " + p.x + p.y);
     }
     public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (double len) {
-        return len * len;
-    }
-    public static double area (double a, double b) {
-      return a * b;
-    }
+
+
 }

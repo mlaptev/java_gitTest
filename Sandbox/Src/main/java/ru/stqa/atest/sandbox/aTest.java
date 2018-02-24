abe738f6ad811f0be3d1778eb6dda8e1ce59b110
mlaptev <mihan22@gmail.com>
2018-02-24 20:26:54 +0600

    ааа

--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
@@ -3,7 +3,7 @@ package ru.stqa.atest.sandbox;
 public class aTest {
 
     static public void main(String[] args) {
-        System.out.println("Hello, Wolder");
+        System.out.println("Hello, Wolderолраыв");
     }
 
 

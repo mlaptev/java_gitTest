bb5dd0913658b88c5816c7598ebe394f8e08ee92
mlaptev <mlaptev@issart.com>
2018-02-24 20:11:55 +0600

    автотест точки

--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/Point.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/Point.java
@@ -1,16 +1,38 @@
 package ru.stqa.atest.sandbox;
 
+//public static double distance(x, y)
+
+
 public class Point {
-    public double x;
-    public double y;
+
+
+
+        public double x;
+        public double y;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+
     }
-    public double area () {
+
+    double distance(Point p) {
+
+        double dx = this.x - x;
+        double dy = this.y - y;
+
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    //public distance (Point p){
+    public double distance() {
         return this.x * this.y;
     }
-}
 
+    // public double distance (Point p){
+
+    //  return distance(p.x, p.y);
+
+
+}
 
--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
@@ -19,57 +19,40 @@ public class aTest {
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
         Point p = new Point( 2, 4);
-                System.out.println("Координаты точек = " + p.x + p.y);
-    }
-    public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-    }
+                System.out.println("Координаты точек = " + p.x + " и " +  p.y);
 
-
-
-}
-
-
-
-class Point { int х, у;
-
-    Point(int х, int у) {
-
-        this.x = х;
-
-        this. y = y;
-
-    }
-
-    double distance(int х, int у) {
-
-        int dx = this.x - х;
-
-        int dy = this.у - у;
-
-        return Math.sqrt(dx*dx + dy*dy);
-
-    }
-
-    double distance(Point p) {
-
-        return distance(p.x, p.y);
-
-    } }
-
-class PointDist {
-
-    public static void main(String args[]) {
-
-        Point p1 = new Point(0, 0);
+        Point p1 = new Point(4, 1);
 
         Point p2 = new Point(30, 40);
 
-        System.out.println("p1 = " + pi.x + ", " + p1.y);
+
+
+        System.out.println("p1 = " + p1.x + ", " + p1.y);
 
         System.out.println("p2 = " + p2.x + ", " + p2.y);
 
         System.out.println("p1.distance(p2) = " + p1.distance(p2));
 
-        System.out.println("p1.distance(60, 80) = " + p1.distance(60, 80));
+        //System.out.println("квадратный корень = " + Math.sqrt(p1));
+
+       // System.out.println("p1.distance(60, 80) = " + p1.distance(60, 80));
+
+
+
+    }
+    public static void hello(String somebody) {
+    System.out.println("Hellio, " + somebody + "!");
+    }
+
+    public static double getDistance(int x1, int y1, int x2, int y2)
+    {
+        //Напишите тут ваш код
+        return Math.sqrt((x2*x2-2*x1*x2+x1*x1) + (y2*y2-2*y1*y2+y1*y1));
+    }
+
+}
+
+
+
+
 

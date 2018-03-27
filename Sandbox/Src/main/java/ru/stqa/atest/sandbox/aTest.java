1dc2c5be5d44a56acfca3d05419c00fc41548455
mlaptev <mlaptev@issart.com>
2018-03-27 15:44:09 +0600

    Расстояние

--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
@@ -18,12 +18,12 @@ public class aTest {
         Rectangle r = new Rectangle(1, 1);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        Point p = new Point( 2, 2);
+        Point p = new Point( 3, 3);
                 System.out.println("Координаты точек = " + p.x + " и " +  p.y);
 
-        Point p1 = new Point(1, 1);
+        Point p1 = new Point(5, 5);
 
-        Point p2 = new Point(2, 2);
+        Point p2 = new Point(3, 3);
 
 
 
@@ -31,7 +31,7 @@ public class aTest {
 
         System.out.println("p2 = " + p2.x + ", " + p2.y);
 
-        System.out.println("p1.distance(p2) = " + p1.distance(p2));
+        System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
 
         //System.out.println("квадратный корень = " + Math.sqrt(p1));
 

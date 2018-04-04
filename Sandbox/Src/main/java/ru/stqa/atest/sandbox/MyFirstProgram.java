f790b90487df77d293a396f1e016a7da92f7cf35
mlaptev <mlaptev@issart.com>
2018-04-04 12:23:53 +0600

    Исправление названия класса

--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/MyFirstProgram.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/MyFirstProgram.java
@@ -1,13 +1,9 @@
 package ru.stqa.atest.sandbox;
 
-import javafx.scene.media.SubtitleTrack;
-
-import java.net.SocketPermission;
-
-public class aTest {
-
+public class MyFirstProgram {
 
     public static void main(String[] args) {
+        hello("wold");
 
 
         Square s = new Square(5);
@@ -16,13 +12,12 @@ public class aTest {
         Rectangle r = new Rectangle(1, 1);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        Point p = new Point( 3, 3);
-                System.out.println("Координаты точек = " + p.x + " и " +  p.y);
+        Point p = new Point(3, 3);
+        System.out.println("Координаты точек = " + p.x + " и " + p.y);
 
         Point p1 = new Point(5, 5);
 
         Point p2 = new Point(3, 3);
-
 
 
         System.out.println("p1 = " + p1.x + ", " + p1.y);
@@ -30,21 +25,8 @@ public class aTest {
         System.out.println("p2 = " + p2.x + ", " + p2.y);
 
         System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
-
-        //System.out.println("квадратный корень = " + Math.sqrt(p1));
-
-       // System.out.println("p1.distance(60, 80) = " + p1.distance(60, 80));
-
-
+    }
+    public static void hello (String somebody) {
 
     }
-
-
-    }
-
-
-
-
-
-
-
+}

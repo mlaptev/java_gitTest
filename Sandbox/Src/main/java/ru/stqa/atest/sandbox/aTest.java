3b0d87ffd7746686c17ebb991e73e4df9f6f476b
mlaptev <mihan22@gmail.com>
2018-04-06 19:33:32 +0600

    Revert "Исправление названия класса"
    This reverts commit f790b90487df77d293a396f1e016a7da92f7cf35.

--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/aTest.java
@@ -1,9 +1,13 @@
 package ru.stqa.atest.sandbox;
 
-public class MyFirstProgram {
+import javafx.scene.media.SubtitleTrack;
+
+import java.net.SocketPermission;
+
+public class aTest {
+
 
     public static void main(String[] args) {
-        hello("wold");
 
 
         Square s = new Square(5);
@@ -12,12 +16,13 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(1, 1);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        Point p = new Point(3, 3);
-        System.out.println("Координаты точек = " + p.x + " и " + p.y);
+        Point p = new Point( 3, 3);
+                System.out.println("Координаты точек = " + p.x + " и " +  p.y);
 
         Point p1 = new Point(5, 5);
 
         Point p2 = new Point(3, 3);
+
 
 
         System.out.println("p1 = " + p1.x + ", " + p1.y);
@@ -25,8 +30,21 @@ public class MyFirstProgram {
         System.out.println("p2 = " + p2.x + ", " + p2.y);
 
         System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
-    }
-    public static void hello (String somebody) {
+
+        //System.out.println("квадратный корень = " + Math.sqrt(p1));
+
+       // System.out.println("p1.distance(60, 80) = " + p1.distance(60, 80));
+
+
 
     }
-}
+
+
+    }
+
+
+
+
+
+
+

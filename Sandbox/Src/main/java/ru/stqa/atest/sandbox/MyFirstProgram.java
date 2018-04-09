8860634946d70c3228e2920165b6dc41c9f5ea84
mlaptev <mlaptev@issart.com>
2018-04-09 10:59:39 +0600

    Исправления

--- Sandbox/Src/main/java/ru/stqa/atest/sandbox/MyFirstProgram.java
+++ Sandbox/Src/main/java/ru/stqa/atest/sandbox/MyFirstProgram.java
@@ -21,10 +21,10 @@ public class MyFirstProgram {
 
 
         System.out.println("p1 = " + p1.x + ", " + p1.y);
-
         System.out.println("p2 = " + p2.x + ", " + p2.y);
-
         System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
+
+
     }
     public static void hello (String somebody) {
 
